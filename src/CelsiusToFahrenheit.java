e3ae8458fdf7ed0ac5244ab1cb12773d49bc8554
EnesKula <100785101+EnesKula@users.noreply.github.com>
2022-03-13 20:25:58 +0300

    EneX ödevini yapar.

--- src/CelsiusToFahrenheit.java
+++ src/CelsiusToFahrenheit.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class CelsiusToFahrenheit {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.print("""
                 Sıcaklık Dönüştürücü
                 1.Santigrad - Fahrenheit
