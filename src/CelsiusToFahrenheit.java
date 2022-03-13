8d9dcc994cbf61cb4d421d8e70a4ff4a9d79089e
Yasin Ekin <ekinyasin90@gmail.com>
2022-03-13 19:59:48 +0300

    All Homeworks were completely done

--- src/CelsiusToFahrenheit.java
+++ src/CelsiusToFahrenheit.java
@@ -1,25 +1,38 @@
+import java.util.Scanner;
+
 public class CelsiusToFahrenheit {
     public static void main(String[] args) {
-        //TODO: Klavyeden girilen Sıcaklık değerleri, diğer birime dönüştürülecektir.
-        /*
-            Örnek Menü Tasarımı:
 
-            Sıcaklık Dönüşüm Uygulaması
-            1. Santigrat - Fahrenheit
-            2. Fahrenheit - Santigrat
-            Seçim :
-            -------------------------
-            Formül: F=Cx1.8+32
-                    C=(F-32)/1.8
-             -----------------------
-            Örnek;  F=30x1.8+32=86
-                    C=(86-32)/1.8=30
-            -------------------------
-             Ekran Çıktısı:
-             30 C --> 86 F
-             86 F --> 30 C
-         */
 
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("TEMPERATURE CONVERSION APPLICATION");
+        System.out.println("------------------------------------");
+        System.out.println("1.Centigrade to Fahrenheit");
+        System.out.println("2.Fahrenheit to Centigrade");
+
+        while(true) {
+            System.out.print("Please choose any one to make a convertion:");
+
+            int choise = scan.nextInt();
+            double f, c;
+            if (choise == 1) {
+                System.out.print("Please enter the degree of centrigrade: ");
+                c = scan.nextDouble();
+                f = (c * 1.8) + 32;
+                System.out.println(" The result of convertion :" + c + " centigrade is " + f + " fahrenheit");
+            } else if (choise == 2) {
+                System.out.print("Please enter the degree of fahrenheit:");
+                f = scan.nextDouble();
+                c = (f - 32) / 1.8;
+                System.out.println(" The result of convertion : " + f + "fahrenheit is " + c + " centigrade");
+
+            } else {
+
+                System.out.println("You made a wrong choise! Please enter again:");
+
+            }
+        }
 
     }
 }
--- src/Stars.java
+++ src/Stars.java
@@ -2,23 +2,20 @@ import java.util.Scanner;
 
 public class Stars {
     public static void main(String[] args) {
-        //TODO: Klavyeden girilen sayı kadar yıldızı ekrana tablo halinde yazan bir program yazınız.
-        /*
-            Ekran:
-            Yıldız Adedi:
-            Sütun Adedi :
 
 
-            Örnek;
-            Yıldız Adedi: 54
-            Sütun Adedi : 12
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*
-         */
+     Scanner scan =new Scanner(System.in);
+        System.out.print("Please enter a column number: ");
+        int column= scan.nextInt();
+        System.out.print("Please enter the number of stars: ");
+        int star= scan.nextInt();
 
+        for(int i=1;i<=star;i++) {
+            System.out.print("*"+"\t");
+            if(i%column<=0){
+                System.out.println(" ");
+            }
+        }
 
     }
 }
