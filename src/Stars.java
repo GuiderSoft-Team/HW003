24fd6a07c1bc14fbff1a0883dda71f76a5ed5975
BİLAL <bllsfk.3984@gmail.com>
2022-03-15 15:52:24 +0300

    Bilal ödevini tamamladı.

--- src/Stars.java
+++ src/Stars.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 
 public class Stars {
     public static void main(String[] args) {
-        //TODO: Klavyeden girilen sayı kadar yıldızı ekrana tablo halinde yazan bir program yazınız.
-        /*
-            Ekran:
-            Yıldız Adedi:
-            Sütun Adedi :
 
-
-            Örnek;
-            Yıldız Adedi: 54
-            Sütun Adedi : 12
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*	*	*	*	*	*	*
-            *	*	*	*	*	*
-         */
-
-
+        Scanner input=new Scanner(System.in);
+                System.out.print("Lütfen Yildiz Adedi Giriniz :");
+        int yildizAdedi=input.nextInt();
+                System.out.print("Lütfen Sütun Adedi Giriniz  :");
+        int sütunAdedi=input.nextInt();
+        for (int i = 1; i <=yildizAdedi; i++) {
+                System.out.print("\t*");
+            if(i%sütunAdedi==0) {
+                System.out.print("\n");
+            }
+        }
     }
 }
+        
+        
+        
+
+
+
