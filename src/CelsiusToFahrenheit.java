0a46a8adfcced7c8d2b03e1ea5f7cf71e1b72c89
Zeynep Düzen <99576262+ZeynepDuzen@users.noreply.github.com>
2022-03-16 13:20:40 -0700

    zeynep's homework

--- src/CelsiusToFahrenheit.java
+++ src/CelsiusToFahrenheit.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CelsiusToFahrenheit {
     public static void main(String[] args) {
         //TODO: Klavyeden girilen Sıcaklık değerleri, diğer birime dönüştürülecektir.
@@ -19,7 +21,36 @@ public class CelsiusToFahrenheit {
              30 C --> 86 F
              86 F --> 30 C
          */
+        System.out.println("Sıcaklık Dönüşüm Uygulaması\n1. Santigrat - Fahrenheit\n2. Fahrenheit - Santigrat\nSeçim :");
+        Scanner input=new Scanner(System.in);
+        int choice=input.nextInt();
 
+        if(choice==1){
+            System.out.println("Santigrat girişi yapınız : ");
+            double santi= input.nextDouble();
+            celciustoFahrenheit(santi);
+        }else if (choice==2){
+            System.out.println("Fahrenheit girişi yapınız : ");
+            double fahren=input.nextDouble();
+            fahrenheittoCelcius(fahren);
+        }else{
+            System.out.println("Geçerli bir seçim yapmadınız. ");
+        }
 
     }
+
+    public static void celciustoFahrenheit (double celcius){
+        double fahrenheit=(celcius*1.8)+32;
+        System.out.println("Celcius  = "+celcius+"  =>  "+"Fahrenheit = "+fahrenheit);
+    }
+
+    public static void fahrenheittoCelcius (double fahrenheit) {
+        double celcius=(fahrenheit-32)/1.8;
+        System.out.println("Fahrenheit = "+fahrenheit+"  =>  "+"Celcius  = "+celcius);
+
+    }
+
+
+
+
 }
--- src/Notlar.java
+++ src/Notlar.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Notlar {
     public static void main(String[] args) {
         //TODO: Klavyeden girilen vize ve final notlarının ortalamasını ve harf notunu bulan bir program yazınız.
@@ -12,12 +14,40 @@ public class Notlar {
                 C 70-79
                 D 60-69
                 F 0-59
-
             Örnek Ekran Çıktısı:
-
             Not : 80 [B]
-
          */
+        System.out.println("Ortalama hesaplama programı : ");
+        Scanner input=new Scanner(System.in);
+        System.out.println("Lütfen vize notunuzu giriniz : ");
+        double vize=input.nextDouble();
+        System.out.println("Lütfen final notunuzu giriniz :");
+        double fnl= input.nextDouble();
+        double ortalama=average(vize,fnl);
+
+        if (ortalama>=90&ortalama<=100){
+            System.out.println("not = "+ortalama);
+            System.out.println("harf notu A ");
+        } else if (ortalama>=80&ortalama<90){
+            System.out.println("not = "+ortalama);
+            System.out.println("harf notu B ");
+        } else if (ortalama>=70&ortalama<80){
+            System.out.println("not = "+ortalama);
+            System.out.println("harf notu C ");
+        } else if (ortalama>=60&ortalama<70){
+            System.out.println("not = "+ortalama);
+            System.out.println("harf notu D ");
+        } else {
+            System.out.println("not = "+ortalama);
+            System.out.println("harf notu F ");
+        }
+
+
 
     }
+    public static double average(double vize,double fnl){
+        return (vize*0.4)+(fnl*0.6);
+    }
+
+
 }
