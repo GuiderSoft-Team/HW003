65f2271d97e7b97b8e2bd7ff95c63590a91a9c69
stokatlio2012 <stokatlio2012@gmail.com>
2022-03-19 01:15:34 +0300

    ödev tamamlandı

--- src/CelsiusToFahrenheit.java
+++ src/CelsiusToFahrenheit.java
@@ -1,3 +1,6 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class CelsiusToFahrenheit {
     public static void main(String[] args) {
         //TODO: Klavyeden girilen Sıcaklık değerleri, diğer birime dönüştürülecektir.
@@ -19,7 +22,24 @@ public class CelsiusToFahrenheit {
              30 C --> 86 F
              86 F --> 30 C
          */
-
-
+        System.out.println("*\t*\tSıcaklık Dönüşüm\t*\t*");
+        System.out.println("Lütfen dönüşüm yapmak istediğiniz sıcaklık birimlerini seçiniz.");
+        System.out.println("1. Santigrat - Fahrenheit");
+        System.out.println("2. Fahrenheit - Santigrat");
+        Scanner input1=new Scanner(System.in);
+        System.out.print("Seçim : ");
+        double secim= input1.nextDouble();
+        Scanner input2=new Scanner(System.in);
+        System.out.print("Sıcaklık Değeri: ");
+        double sicaklik= input2.nextDouble();
+        if(secim==1){
+            double F=(sicaklik*1.8)+32;
+            System.out.println(sicaklik+"C Derece -->"+F+" Fahrenheit");
+        }else if(secim==2){
+            double C=(sicaklik-32)/1.8;
+            System.out.println(sicaklik+" Fahrenheit -->"+C+" C derece");
+        }else{
+            System.err.println("Hatalı parametre girişi!");
+        }
     }
 }
