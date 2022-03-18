03db99a2281f199e3308a0d292a6697f0fe63d06
stokatlio2012 <stokatlio2012@gmail.com>
2022-03-19 00:41:55 +0300

    Yıldız ve Ortalama Not ödevleri yapıldı.

--- src/Stars.java
+++ src/Stars.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Stars {
     public static void main(String[] args) {
-        //TODO: Klavyeden girilen sayı kadar yıldızı ekrana tablo halinde yazan bir program yazınız.
         /*
             Ekran:
             Yıldız Adedi:
@@ -18,7 +17,27 @@ public class Stars {
             *	*	*	*	*	*	*	*	*	*	*	*
             *	*	*	*	*	*
          */
-
-
+        System.out.println("Lütfen ekrana yazdırmak istediğiniz yıldız sütunları bilgilerini giriniz.");
+        Scanner input1=new Scanner(System.in);
+        System.out.print("Yıldız adedi: ");
+        int star=input1.nextInt();
+        Scanner input2=new Scanner(System.in);
+        System.out.print("Sütün adedi: ");
+        int column=input2.nextInt();
+        int sayac=0;
+        for(int i=0;i<star;i++) {
+            for (int k=0;k<column;k++){
+                System.out.print("*\t");
+                sayac++;
+                if (sayac==star){
+                    break;
+                }
+            }
+            if (sayac==star){
+                break;
+            }
+            System.out.println();
+        }
     }
 }
+
