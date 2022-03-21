8c8461b5c18b168d9449a390887e3cd86058fcf0
gazimurat <yemliha.cooker@gmail.com>
2022-03-21 23:14:27 +0300

    HW003 tamamlandı

--- src/CelsiusToFahrenheit.java
+++ src/CelsiusToFahrenheit.java
@@ -1,6 +1,6 @@
 public class CelsiusToFahrenheit {
     public static void main(String[] args) {
-        //TODO: Klavyeden girilen Sıcaklık değerleri, diğer birime dönüştürülecektir.
+        //Klavyeden girilen Sıcaklık değerleri, diğer birime dönüştürülecektir.
         /*
             Örnek Menü Tasarımı:
 
@@ -20,6 +20,23 @@ public class CelsiusToFahrenheit {
              86 F --> 30 C
          */
 
+        System.out.println("76F---> "+convertFahrenheittoCelcius(76)+"C");
+        System.out.println("50F---> "+convertFahrenheittoCelcius(50)+"C");
+
+        int f=80;
+        System.out.println(String.format("%d F---> %5.1f C",f,convertFahrenheittoCelcius(f)));
+
+        convertFahrenheittoCelcius(95.8);
+
 
     }
-}
+
+    public static double convertFahrenheittoCelcius(double grade) {
+        return (grade-32)/1.8;
+    }
+        public static double convertFahrenheittoCelcius(int grade){
+            return (grade-32)/1.8;
+        }
+
+    }
+
