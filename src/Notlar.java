89740108957e3e124d6fb649b897e92f0442ef4c
90551 <akarsu64engin@gmail.com>
2022-03-21 15:21:32 +0300

    home work 3

--- src/Notlar.java
+++ src/Notlar.java
@@ -8,6 +8,11 @@ public class Notlar {
         int vizeNotu=input.nextInt();
         System.out.print("Final Notu = ");
         int finalNotu=input.nextInt();
+        if(vizeNotu<0|finalNotu<0){
+            System.err.println("Girilen Not Eksi Olamaz");
+            System.out.println("Lütfen Tekrar Giriniz");
+            System.exit(-1);
+        }
 
         double ortalama=(vizeNotu*0.4)+(finalNotu*0.6);
         String notOrtHarf= ortalama < 60 ? ortalama + " [" + "F" + "]" : ortalama < 70 ? ortalama + " [" + "D" + "]" : ortalama < 80 ? ortalama + " [" + "C" + "]" : ortalama < 90 ? ortalama + " [" + "B" + "]" : ortalama <= 100 ? ortalama + " [" + "A" + "]" : "geçersiz bir ortalama girildi";
