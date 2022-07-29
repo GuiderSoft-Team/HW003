a5fcf0e9b1512ae3834375bc1fbaaac1cd0a202a
nizamimtest <nizamimtest@gmail.com>
2022-07-29 13:44:43 +0400

    nnot

--- src/Stars.java
+++ src/Stars.java
@@ -19,6 +19,43 @@ public class Stars {
             *	*	*	*	*	*
          */
 
-
+      /* System.out.print("Yilidz Adedi: ");
+        int yildiz = scan.nextInt();
+        System.out.print("Sutun Adedi: ");
+        int sutun=scan.nextInt();
     }
 }
+*/
+      /*  Scanner scan = new Scanner(System.in);
+       System.out.print("Yilidz Adedi: ");
+        int yildiz = scan.nextInt();
+        System.out.print("Sutun Adedi: ");
+        int sutun = scan.nextInt();
+        while (yildiz>0);
+        int i = 0;
+            for (i = 0; i >= sutun; i++) ;
+        int j;
+            for (j = 0; j >= yildiz; j++) {
+            System.out.println("*");
+        }
+        System.out.println("*");*/
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yıldız Adedi : ");
+        int yildiz = input.nextInt();
+        System.out.print("Sütün Adedi : ");
+        int sutun = input.nextInt();
+        while (yildiz > 0) {
+            String a = "";
+            for (int i = 0; i < sutun; i++) {
+                if (yildiz > 0) {
+                    a = a + " *";
+                }
+                yildiz = yildiz - 1;
+            }
+            System.out.println(a);
+        }
+    }
+}
+
+
+
